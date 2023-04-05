dda0b8a8eeb19a810e001a7866582cef547c7084
Zharfan0 <zharfanwidyadana@gmail.com>
2023-04-05 11:30:23 +0700

    update

--- struct3/struct3.cpp
+++ struct3/struct3.cpp
@@ -22,24 +22,25 @@ int main() {
 	{
 		cout << "Masukkan nim: ";
 		cin.getline(mhs[i].nim, 12);
-		cout << "Masukkan nama: ";
-		cin.getline(mhs[i].nama, 20);
-		cout << "Alamat: " << endl;
-		cout << "\tNama desa : ";
-		cin.getline(mhs[i].alamat.desa, 20);
-		cout << "\tNama kota : ";
-		cin.getline(mhs[i].alamat.kota, 20);
 		cout << "Masukkan umur: ";
 		cin >> mhs[i].umur;
 		cin.ignore(1, '\n');
+		cout << "Masukkan nama: ";
+		cin.getline(mhs[i].nama, 20);
+		cout << "Alamat: " << endl;
+		cout << "\tMasukan desa : ";
+		cin.getline(mhs[i].alamat.desa, 20);
+		cout << "\tMasukan kota : ";
+		cin.getline(mhs[i].alamat.kota, 20);
 	}
 
 	for (int i = 0; i < 3; i++)
 	{
 		cout << "\nNim : " << mhs[i].nim;
+		cout << "\numur : " << mhs[i].umur;
 		cout << "\nnama : " << mhs[i].nama;
+		cout << "\nalamat : ";
 		cout << "\ndesa : " << mhs[i].alamat.desa;
 		cout << "\nkota : " << mhs[i].alamat.kota;
-		cout << "\numur : " << mhs[i].umur;
 	}
 }
