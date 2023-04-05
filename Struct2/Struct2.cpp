ac2521f65e8a67557ff2f98170d0a6e26318c020
Zharfan0 <zharfanwidyadana@gmail.com>
2023-04-05 10:55:24 +0700

    update

--- Struct2/Struct2.cpp
+++ Struct2/Struct2.cpp
@@ -1,40 +1,45 @@
 #include <iostream>
 
 using namespace std;
-
 struct AlamatDetail
 {
-	string desa;
-	string kota;
-
-
+	char desa[20];
+	char kota[20];
 };
 
 struct Mahasiswa {
-	string nim;
-	string nama;
+	char nim[12];
+	char nama[20];
 	AlamatDetail alamat;
 	int umur;
 };
 
 int main() {
 
-	Mahasiswa mhs;
+	Mahasiswa mhs[3];
 
-	cout << "Masukkan nim: ";
-	cin >> mhs.nim;
-	cout << "Masukkan nama: ";
-	cin >> mhs.nama;
-	cout << "Alamat: " << endl;
-	cout << "\tNama desa : ";
-	cin >> mhs.alamat.desa;
-	cout << "\tNama kota: ";
-	cin >> mhs.alamat.kota;
-	cout << "Masukkkan umur: ";
-	cin >> mhs.umur;
+	for (int i = 0; i < 3; i++)
+	{
+		cout << "Masukkan nim: ";
+		cin.getline(mhs[i].nim, 12);
+		cout << "Masukkan nama: ";
+		cin.getline(mhs[i].nama, 20);
+		cout << "Alamat: " << endl;
+		cout << "\tNama desa : ";
+		cin.getline(mhs[i].alamat.desa, 20);
+		cout << "\tNama kota : ";
+		cin.getline(mhs[i].alamat.kota, 20);
+		cout << "Masukkan umur: ";
+		cin >> mhs[i].umur;
+		cin.ignore(1, '\n');
+	}
 
-	cout << "\nNim : " << mhs.nim;
-	cout << "\nnama : " << mhs.nama;
-	cout << "\ndesa : " << mhs.alamat.desa;
-	cout << "\nkota : " << mhs.alamat.kota;
+	for (int i = 0; i < 3; i++)
+	{
+		cout << "\nNim : " << mhs[i].nim;
+		cout << "\nnama : " << mhs[i].nama;
+		cout << "\ndesa : " << mhs[i].alamat.desa;
+		cout << "\nkota : " << mhs[i].alamat.kota;
+		cout << "\numur : " << mhs[i].umur;
+	}
 }
