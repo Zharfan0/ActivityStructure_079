379addaef26eba6786b640fb13a17997c3dfd4aa
Zharfan0 <zharfanwidyadana@gmail.com>
2023-04-05 10:37:06 +0700

    int main lengkapp

--- struct/struct.cpp
+++ struct/struct.cpp
@@ -8,3 +8,33 @@ struct Mahasiswa {
 	string alamat;
 	int umur;
 };
+
+int main()
+{
+	Mahasiswa mhs1, mhs2;
+
+	mhs1.nim = "20220140079";
+	mhs1.nama = "Japran";
+	mhs1.alamat = "Jaksel";
+	mhs1.umur = 17;
+
+	cout << "Masukkan nim: ";
+	cin >> mhs2.nim;
+	cout << "Masukkan nama: ";
+	cin >> mhs2.nama;
+	cout << "Masukkan alamat: ";
+	cin >> mhs2.alamat;
+	cout << "Masukkan umur: ";
+	cin >> mhs2.umur;
+
+	cout << "\nNim : " << mhs1.nim;
+	cout << "\nnama : " << mhs1.nama;
+	cout << "\nalamat : " << mhs1.alamat;
+	cout << "\numur : " << mhs1.umur;
+
+	cout << "\nNim : " << mhs2.nim;
+	cout << "\nnama : " << mhs2.nama;
+	cout << "\nalamat : " << mhs2.alamat;
+	cout << "\numur : " << mhs2.umur;
+
+}
